8ffe9f7c4479ca9834fbf93349746c9b895e1d94
heba189 <Ultimate@DESKTOP-1QGBQMI>
2024-06-04 16:45:26 +0300

    end section 10

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f98643f62cca3063b59c87c2611f7a8dc906b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ebb4153099e044bd86ae3dfe1b6cb81787da75")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
